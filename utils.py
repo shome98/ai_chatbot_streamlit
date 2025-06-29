078c1987698ac833f377a5a9460cbb20a5f041a1
Sowparna Shome <soupornoshome@gmail.com>
2025-06-30 01:49:12 +0530

    update to use secrets

--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
+import streamlit as st
 from datetime import datetime
 from pymongo import MongoClient
 from passlib.hash import bcrypt
 import os
 import hashlib
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+# MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI =st.secrets["MONGO_URI"]
 client = MongoClient(MONGO_URI)
 db = client.chatbot_db_host
 users_collection = db.users
